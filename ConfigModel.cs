e789dadc288a049c3c2efccc39b1a165818b0e7e
mjbmb <82882983+mjbmb@users.noreply.github.com>
2025-07-08 20:01:01 +0200

    empty

--- ConfigModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ObsidianGitMirror
-{
-    internal class ConfigModel
-    {
-        public string RepositoryPath { get; set; } = "";
-        public string VaultOutputPath { get; set; } = "";
-    }
-}
